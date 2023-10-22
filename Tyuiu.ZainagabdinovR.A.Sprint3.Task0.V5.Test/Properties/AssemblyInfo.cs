f11ed2a9b6b805f38b059658c246333b2f2cd10a
Tyuiu.ZainagabdinovR.A@outlook.com <Ruslanzai2003@gmail.com>
2023-10-22 18:09:02 +0500

    Task0

---
+++ Tyuiu.ZainagabdinovR.A.Sprint3.Task0.V5.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ZainagabdinovR.A.Sprint3.Task0.V5.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ZainagabdinovR.A.Sprint3.Task0.V5.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("dd1677a0-8d17-4cc7-96c5-58040f953244")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
