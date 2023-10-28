34a241b93d648d4a2c7975b3ff5f94f17732e37f
Tyuiu.ZainagabdinovR.A@outlook.com <Ruslanzai2003@gmail.com>
2023-10-28 21:32:14 +0500

    Task6

---
+++ Tyuiu.ZainagabdinovR.A.Sprint3.Task6.V9.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ZainagabdinovR.A.Sprint3.Task6.V9.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ZainagabdinovR.A.Sprint3.Task6.V9.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1718946b-4daf-4a58-b454-64f320172086")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.ZainagabdinovR.A.Sprint3.Task6.V9/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.ZainagabdinovR.A.Sprint3.Task6.V9.Lib;
+
+namespace Tyuiu.ZainagabdinovR.A.Sprint3.Task6.V9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Зайнагабдинов Р. А. | ИСТНб-23-1";
+            //Длина строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Использование операторов continue и break в циклах                *");
+            Console.WriteLine("* Задание #3.6                                                            *");
+            Console.WriteLine("* Вариант #9                                                              *");
+            Console.WriteLine("* Выполнил: Зайнагабдинов Руслан Айратович | ИСТНб-23-1                   *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Напишите программу, которая ищет среди целых чисел,                     *");
+            Console.WriteLine("* принадлежащих числовому отрезку [19, 30] количество всех делителей      *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int startValue = 19;
+            int stopValue = 30;
+
+            Console.WriteLine("Начало отрезка = " + startValue);
+            Console.WriteLine("Конец отрезка = " + stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Сумма делителей = " + ds.GetSumTheDivisors(startValue, stopValue));
+
+            Console.ReadKey();
+        }
+    }
+}
